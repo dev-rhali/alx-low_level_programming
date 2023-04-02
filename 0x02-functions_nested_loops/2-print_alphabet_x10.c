4275ba828b6f71c0ddc4712e63ebb1d52808ddb6
soukaina <soukaina@gmail.com>
2023-04-01 17:41:29 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/** 
+ * main - print alphabet x10
+ *
+ * Return Always 0 Success
+ */
+
+void print_alphabet_x10(void)
+{
+	int i , j ;
+
+	for(i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+		{
+			_putchar(j);
+		}
+	_putchar('\n');
+	}
+}
